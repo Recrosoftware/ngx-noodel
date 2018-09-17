0d43e448d6ace8e6d5dddec90b3fa40693c225c0
Davide Menegatti <d.menegatti@recrosoftware.com>
2018-09-17 09:38:22 +0200

    - Items validation (fix)

--- projects/naisc-core/src/public_api.ts
+++ projects/naisc-core/src/public_api.ts
@@ -7,7 +7,9 @@ export * from './lib/naisc.module';
 
 export * from './lib/shared/naisc-dump';
 export * from './lib/shared/naisc-events';
+export * from './lib/shared/naisc-extent';
 export * from './lib/shared/naisc-item';
 export * from './lib/shared/naisc-item-content';
 export * from './lib/shared/naisc-item-descriptor';
 export * from './lib/shared/naisc-item-options';
+export * from './lib/shared/naisc-validation';
